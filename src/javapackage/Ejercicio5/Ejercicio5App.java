7a87000f91945ff63012ab097af15fa75a04ed2c
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-01-11 16:49:23 +0100

    Exercises 5

--- src/javapackage/Ejercicio5/Ejercicio5App.java
+++ src/javapackage/Ejercicio5/Ejercicio5App.java
@@ -21,6 +21,18 @@ A tome el valor de D
 •
 D tome el valor de B
 EJERCICIOS*/
+		
+		int A = 7, B = 6, C = 5, D = 4;
+		
+		System.out.println("Los valores iniciales de las variables son: \nA = " + A + "\nB = " + B + "\nC = " + C + "\nD = " + D);
+		
+		System.out.println("Ahora B tiene el valor " + (B = C));
+		System.out.println("Ahora C tiene el valor " + (C = A));
+		System.out.println("Ahora A tiene el valor " + (A = D));
+		System.out.println("Ahora D tiene el valor " + (D = B));
+		
+		System.out.println("Los valores actuales de las variables son: \nA = " + A + "\nB = " + B + "\nC = " + C + "\nD = " + D);
+		
 	}
 
 }
