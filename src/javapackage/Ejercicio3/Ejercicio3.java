37883f8f0fd5328168261f2a1b0e3cd0447c6dab
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-01-11 13:44:50 +0100

    Ejercicio 3

--- src/javapackage/Ejercicio3/Ejercicio3.java
+++ src/javapackage/Ejercicio3/Ejercicio3.java
@@ -20,6 +20,7 @@ El cociente X / Y
 •
 El resto X % Y
 •
+
 La suma N + M
 •
 La diferencia N M
@@ -30,6 +31,7 @@ El cociente N / M
 •
 El resto N % M
 •
+
 La suma X + N
 •
 El cociente Y / M
@@ -47,7 +49,43 @@ EJERCICIOS*/
 		
 		double N = 5, M = 6;
 		
+		System.out.println("El valor de la variable X : " + X + "\nEl valor de la variable Y : " + Y + "\nEl valor de la variable N : " + N + "\nEl valor de la variable M : " + M);
 		
+		System.out.println("La suma X + Y =  " + (X + Y));
+		
+		System.out.println("La diferencia X Y =  " + (X - Y));
+		
+		System.out.println("El producto X * Y =  " + (X * Y));
+		
+		System.out.println("El cociente X / Y =  " + (X / Y));
+		
+		System.out.println("El resto X % Y =  " + (X % Y));
+		
+		System.out.println("El resto X % Y =  " + (X % Y));
+		
+		
+		System.out.println("La suma N + M = " + (N + M));
+		
+		System.out.println("La diferencia N M = " + (N - M));
+		
+		System.out.println("El producto N * M = " + (N * M));
+		
+		System.out.println("El cociente N / M = " + (N / M));
+		
+		System.out.println("El resto N % M =" + (N % M));
+		
+		
+		System.out.println("La suma X + N  = " + (X + N));
+		
+		System.out.println("El cociente Y / M  = " + (Y / M));
+		
+		System.out.println("El resto Y % M  =  " + (Y % M));
+		
+		System.out.println("El doble de cada variable:  \n X =  " + (X * X) + "\n Y = " + (Y * Y) + "\n N = " + (N * N) + "\n M = " + (M * M));
+		
+		System.out.println("La suma de todas las variables: " + (X + Y + N + M));
+		
+		System.out.println("El producto de todas las variables  " + (X * Y * N * M));
 	}
 
 }
