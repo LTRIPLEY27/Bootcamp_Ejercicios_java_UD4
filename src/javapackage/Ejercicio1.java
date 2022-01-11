1f165a5878583ac3f98363b4a3733e67329fba45
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-01-11 12:47:55 +0100

    Ejercicio 1

--- src/javapackage/Ejercicio1.java
+++ src/javapackage/Ejercicio1.java
@@ -3,7 +3,25 @@ package javapackage;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		 * 1
+Declara dos variables numéricas (con el valor que desees), muestra por consola la suma,
+resta, multiplicación, división y módulo (resto de la división).
+		 * */
+		
+		int a = 9, b = 5, suma = 0, resta = 0, multi = 0, div = 0, res = 0;
+		
+		suma = a + b;
+		resta = a - b;
+		multi = a * b;
+		div = a / b;
+		res = a % b;
+		
+		System.out.println("La suma es igual a = "+ suma);
+		System.out.println("La resta es igual a = " + resta);
+		System.out.println("La multiplicacion es igual a = "+ multi);
+		System.out.println("La division es igual a = " + div);
+		System.out.println("El modulo es igual a = " + res);
 
 	}
 
